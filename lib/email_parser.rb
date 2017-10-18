d55e97781ab2d5b97d5e078c23cdda8f1307c727
Jenna Mandel <jenna.b.mandel@gmail.com>
2017-10-18 21:24:40 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,12 +8,12 @@ attr_accessor :emails
 
 def initialize(emails)
   @emails = emails
-end 
+end
 
 def parse
   emails.split.map do |email|
     email.split(',')
-  end.flatten.unique
+  end.flatten.uniq
 end
 
 end
